d0689fcaee49635300a542ca254e717f3d7790df
minuo9830 <minuo9830@gmail.com>
2022-09-22 21:11:23 +0900

    2022.09.22

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            monster monster1 = new monster();
+            player player1 = new player();
+
+            player1.Addat(10);
+
+            player1.Fight(monster1.at);
+            Console.WriteLine(monster1.at);
+            Console.WriteLine(player1.at);
+            Console.ReadLine();
+
+         
+        }
+    }
+}
---
+++ player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class player
+    {
+        public int at = 10;
+        public int hp = 100;
+
+        public void Addat(int _at)
+        {
+            //공격력 증가하기 위한 함수
+            at += _at;
+        }
+
+        public void Fight(int monsterAt )
+        {
+            hp -= monsterAt;
+        }
+    }
+}
